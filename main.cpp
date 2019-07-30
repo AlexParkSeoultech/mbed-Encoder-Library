63cc9167dfa24cadc72f0e3d5a1348e69fc8baad
AlexParkSeoultech <hjpark0922@gmail.com>
2019-07-30 15:15:26 +0900

    upload source

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mbed.h"
+#include "QEncoder.h"
+QEncoder encoA(D2,D3);
+QEncoder encoB(D4,D5);
+DigitalOut led1(LED1);
+int main() {
+    encoA.init();encoB.init();
+    encoA.setCount(0);encoB.setCount(0);
+    while(1) {
+             printf("A= %d, B= %d \n", encoA.getCount(), encoB.getCount());
+             wait(0.01);
+    }
+}
